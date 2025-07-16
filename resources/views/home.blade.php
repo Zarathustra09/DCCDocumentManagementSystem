2cf334138eed0c710b15cdce4059810798908247
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2025-07-16 14:05:03 +0800

    feat: add padding to the container in home view for improved layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid pt-3">
     <div class="row">
         <div class="col-12">
             <div class="card mb-4">
