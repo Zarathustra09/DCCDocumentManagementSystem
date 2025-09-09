713903788327a10bd10292b87518428739c8be7d
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2025-09-09 11:06:25 +0800

    feat: update admin notification logic to include DCCAdmin role for document registration events

--- app/Notifications/DocumentRegistryEntryCreated.php
+++ app/Notifications/DocumentRegistryEntryCreated.php
@@ -52,7 +52,7 @@ class DocumentRegistryEntryCreated extends Notification
 
     public static function sendToAdmins(DocumentRegistrationEntry $entry)
     {
-        $admins = User::role(['SuperAdmin'])->get();
+        $admins = User::role(['SuperAdmin','DCCAdmin'])->get();
         foreach ($admins as $admin) {
             $admin->notify(new static($entry));
         }
