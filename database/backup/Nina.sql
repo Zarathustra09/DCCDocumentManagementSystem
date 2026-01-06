5b3200e62e5f1955efb6c4941970de1d31ff9830
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2026-01-06 12:32:50 +0800

    created add single seeder

---
+++ database/backup/Nina.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT *
+FROM spears24.employees
+WHERE employee_no = 391
+LIMIT 0, 50000
+
+-- Date: 2026-01-06 12:26
+*/
+INSERT INTO `` (`id`,`employee_no`,`username`,`password`,`firstname`,`middlename`,`lastname`,`address`,`birthdate`,`contact_info`,`gender`,`position_id`,`department_id`,`schedule_id`,`organization_id`,`datehired`,`photo`,`created_on`,`usernameadmin`,`passwordadmin`,`barcode`,`email`,`separationdate`,`userrole`,`useraccess`,`safetycouncil`,`vacationleave`,`sickleave`,`emergencyleave`,`earnVL`,`earnSL`,`earnEL`,`civil_status`,`emergency_contact`,`emergency_number`,`sss_number`,`philhealth_number`,`pagibig_number`,`tin_number`,`work_status`,`cedula_govid`,`rate`) VALUES (379,'391','monica','$2y$10$cO7g.0Wg6eBIgwfIIu6N5OvCR7oRDXi/4dNxZKcYtH8Kf5zm3dhMi','Niña Monica','Barte','Sta. Ana','Sitio Looban Banaba Kanluran, Batangas City','2002-09-22','0976-509-0563','Female',91,12,2,2,'2025-08-01','Untitled design (9).png','2025-08-06','','','','N/A','0000-00-00','0',0,0,0,0,0,0,0,0,'','',0,0,0,0,0,'','',0.00);
