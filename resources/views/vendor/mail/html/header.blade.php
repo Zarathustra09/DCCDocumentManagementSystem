9e6acbb7eaf8aedf950b0774904a3ffcf64567fc
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2026-02-04 15:04:54 +0800

    Feat: Updated vendor.mail.html.header logo to be embedded inside the email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,17 @@
 @props(['url'])
+@php
+    $logoPath = public_path('logo.png');
+    $logoData = file_exists($logoPath) ? base64_encode(file_get_contents($logoPath)) : null;
+    $mimeType = file_exists($logoPath) ? mime_content_type($logoPath) : 'image/png';
+@endphp
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('logo.png') }}" class="logo" alt="{{ config('app.name') }}" style="display: block; height: auto; max-height: 80px; width: auto; max-width: 200px;">
+@if($logoData)
+<img src="data:{{ $mimeType }};base64,{{ $logoData }}" class="logo" alt="{{ config('app.name') }}" style="display: block; height: auto; max-height: 80px; width: auto; max-width: 200px;">
+@else
+{{ config('app.name') }}
+@endif
 </a>
 </td>
 </tr>
