257d5e7d05ecf02c5e7ec9ae222d2ecfafa1e513
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2026-02-04 14:51:35 +0800

    Feat: Updated vendor.mail.html.header and vendor/mail/html/themes/default.css
    - changed the appname to be SPIDMS logo or logo.png
    - changed the default.css logo size to use auto

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{!! $slot !!}
-@endif
+<img src="{{ asset('logo.png') }}" class="logo" alt="{{ config('app.name') }}" style="display: block; height: auto; max-height: 80px; width: auto; max-width: 200px;">
 </a>
 </td>
 </tr>
