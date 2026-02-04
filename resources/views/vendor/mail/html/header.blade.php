c9878336dbbf389fd47c70a2f8f6f4b814e0ea2a
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2026-02-04 16:20:45 +0800

    Feat: reverted the header mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,16 +1,11 @@
 @props(['url'])
-@php
-    $logoPath = public_path('logo.png');
-    $logoData = file_exists($logoPath) ? base64_encode(file_get_contents($logoPath)) : null;
-    $mimeType = file_exists($logoPath) ? mime_content_type($logoPath) : 'image/png';
-@endphp
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if($logoData)
-<img src="data:{{ $mimeType }};base64,{{ $logoData }}" class="logo" alt="{{ config('app.name') }}" style="display: block; height: auto; max-height: 80px; width: auto; max-width: 200px;">
+@if (trim($slot) === 'Laravel')
+<img src="{{ $message->embed(public_path('logo.png')) }}" class="logo" alt="{{ config('app.name') }}" style="display: block; height: auto; max-height: 80px; width: auto; max-width: 200px;">
 @else
-{{ config('app.name') }}
+{{ $slot }}
 @endif
 </a>
 </td>
